8c6d9e90b27b790db0703b11201507f1cac6e705
ttaanaay <t.taanaa.y@gmail.com>
2026-07-31 01:25:42 +0700

    commit

--- app/src/main/java/com/btween/app/ui/components/ReportDialog.kt
+++ app/src/main/java/com/btween/app/ui/components/ReportDialog.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Row
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
